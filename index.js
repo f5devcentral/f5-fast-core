e1cd60ea40d96da89fca6d2118673e38a26b77b9
Mitchell Stokes <M.Stokes@F5.com>
2021-12-17 18:15:39 -0800

    Expose BaseTemplateProvider class

--- index.js
+++ index.js
@@ -18,13 +18,19 @@
 const dataStores = require('@f5devcentral/atg-storage');
 
 const FsSchemaProvider = require('./lib/schema_provider').FsSchemaProvider;
-const { FsTemplateProvider, FsSingleTemplateProvider, DataStoreTemplateProvider } = require('./lib/template_provider');
+const {
+    BaseTemplateProvider,
+    FsTemplateProvider,
+    FsSingleTemplateProvider,
+    DataStoreTemplateProvider
+} = require('./lib/template_provider');
 const { Template, mergeStrategies, postProcessStrategies } = require('./lib/template');
 const guiUtils = require('./lib/gui_utils');
 const TransactionLogger = require('./lib/transaction_logger');
 
 module.exports = {
     FsSchemaProvider,
+    BaseTemplateProvider,
     FsTemplateProvider,
     FsSingleTemplateProvider,
     DataStoreTemplateProvider,
